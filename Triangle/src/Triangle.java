37d72a31c006ea8d1a7692428750767f3ba19bc9
NJSAUND <njsaund@gmail.com>
2018-03-23 14:20:41 -0700

    test

--- Triangle/src/Triangle.java
+++ Triangle/src/Triangle.java
@@ -7,13 +7,15 @@ import java.util.Scanner;
 public class Triangle {
 	public static void main(String[] args) 
 	{
-		Scanner file = new Scanner("input.txt");
+		/*Scanner file = new Scanner("input.txt");
 		//String triangleSide1 = "1 x 1";
 		//Scanner parse = new Scanner (triangleSide1);
 		file.useDelimiter(" ");
-		
+		*/
 		try
 		{
+			
+			
 			int s1= file.nextInt();
 			System.out.println(s1);
 			int s2= file.nextInt();
