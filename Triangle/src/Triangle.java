c23ca563d3ec81d3b21c02e560f9a4a4920ba7a0
NJSAUND <njsaund@gmail.com>
2018-03-23 14:20:34 -0700

    First test

--- Triangle/src/Triangle.java
+++ Triangle/src/Triangle.java
@@ -1,40 +1,31 @@
-
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 
 public class Triangle {
-
-	public static void main(String[] args) {
-						
-		try {
-			Scanner file = new Scanner( new File("Text.txt" ) );
+	public static void main(String[] args) 
+	{
+		Scanner file = new Scanner("input.txt");
+		//String triangleSide1 = "1 x 1";
+		//Scanner parse = new Scanner (triangleSide1);
+		file.useDelimiter(" ");
+		
+		try
+		{
+			int s1= file.nextInt();
+			System.out.println(s1);
+			int s2= file.nextInt();
+			System.out.println(s2);
+			int s3= file.nextInt();
+			System.out.println(s3);
+				
+		}
+		catch(InputMismatchException ime)
+		{
+			System.out.println("Error in data");
 			
 		}
-		
-		
-		//File input = new File("Whatever.txt");
-		//Scanner scan = new Scanner(input);
-		
-		
-		/*if (a == b && b == c) {
-            System.out.println("Equilateral Triangle");
-        } 
-		else {
-            if ((a == b && b !=c) || (a == c && a != b)) {
-                System.out.println("Isoceles Triangle");
-            }
-            if (a != b && b != c && a != c) { 
-                System.out.println("Scalene Triangle");
-            }
-            if (((a + b) > (c)) && ((a + c) > b) && ((b + c) > a)) {
-                System.out.println("Not a Triangle");
-            }
-        }
-		*/		
-		file.close();
-			
 	}
-
 }
-
